53a80c917601372ef76535a5c35e44c11b087bce
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-17 23:26:47 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,5 +8,7 @@ class SongsController < ApplicationController
 
   def set_song
     @song = Song.find(params[:id])
+    @genre = Genre.find(params[:genre_id])
+    @artist = Artist.find(params[:artist_id])
   end
 end
