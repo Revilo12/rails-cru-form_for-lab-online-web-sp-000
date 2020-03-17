98383e1a0a76275a5843ec340203d11fd22597a8
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-17 23:05:35 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,6 +9,8 @@ class ArtistsController < ApplicationController
 
   def create
     @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
 
   private
 
